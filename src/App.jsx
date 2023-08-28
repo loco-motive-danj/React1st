b8d666262fab84b4de08e7e19094e44ca2db6c7c
DanielJerman <danieljerman97@gmail.com>
2023-08-28 17:12:02 -0400

    finished changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
 import {puppyList} from './data.js'
 import './App.css'
+import './index.css'
 import { useState } from 'react'
 
 function App() {
@@ -9,15 +9,33 @@ function App() {
 
   console.log("puppyList: ", puppyList);
 
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
   return (
     <div className="App">
+      <div className = "logo">
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+          return <button onClick= {()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</button>;
+        })
+      }
+       {featPupId && (
+
+          <div className='read-the-docs'>
+            <h1>{featuredPup.name}</h1>
+            <ul>
+              <a>Age: {featuredPup.age}</a>
+              <a>Email: {featuredPup.email}</a>
+            </ul>
+          </div>
+      )} 
+    
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
+
