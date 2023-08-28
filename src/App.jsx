a2b00a5d4341827aea0dc94232c3220fcb7dd9ae
DanielJerman <danieljerman97@gmail.com>
2023-08-28 14:18:40 -0400

    puppy data useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 
-
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      <div>
-      
-      </div>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
